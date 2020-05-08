6972edfba8c9c45116cab62a57e54e099720b6b4
manuelaplesa96 <manuela.plesa@gmail.com>
2020-05-08 14:51:55 +0200

    Kreirane tablice

--- app/database/create_tables.php
+++ app/database/create_tables.php
@@ -52,7 +52,7 @@ function create_table_users()
 
 		$st->execute();
 	}
-	catch( PDOException $e ) { exit( "PDO error [create dz2_users]: " . $e->getMessage() ); }
+	catch( PDOException $e ) { exit( "PDO error [create spiza_users]: " . $e->getMessage() ); }
 
 	echo "Napravio tablicu spiza_users.<br />";
 }
@@ -77,17 +77,97 @@ function create_table_restaurants()
 			'email varchar(50) NOT NULL,' .
 			'registration_sequence varchar(20) NOT NULL,' .
 			'rating int NOT NULL,' .
+			'food_type varchar(50) NOT NULL,' .
 			'description varchar(50) NOT NULL,' .
 			'has_registered int)'
 		);
 
 		$st->execute();
 	}
-	catch( PDOException $e ) { exit( "PDO error [create dz2_channels]: " . $e->getMessage() ); }
+	catch( PDOException $e ) { exit( "PDO error [create dz2_restaurants]: " . $e->getMessage() ); }
 
 	echo "Napravio tablicu spiza_restaurants.<br />";
 }
 
+
+function create_table_food()
+{
+	$db = DB::getConnection();
+
+	if( has_table( 'spiza_food' ) )
+		exit( 'Tablica spiza_food vec postoji. Obrisite ju pa probajte ponovno.' );
+
+	try
+	{
+		$st = $db->prepare( 
+			'CREATE TABLE IF NOT EXISTS spiza_food (' .
+			'id int NOT NULL PRIMARY KEY AUTO_INCREMENT,' .
+			'name varchar(50) NOT NULL,' .
+			'food_type varchar(50) NOT NULL,' .
+			'description varchar(50) NOT NULL,' .
+			'waiting_time int NOT NULL,' .
+			'thumbs_up int NOT NULL,' .
+			'id_restaurant int NOT NULL,' .
+			'price int NOT NULL)'		
+		);
+
+		$st->execute();
+	}
+	catch( PDOException $e ) { exit( "PDO error [create spiza_food]: " . $e->getMessage() ); }
+
+	echo "Napravio tablicu spiza_food.<br />";
+}
+
+function create_table_feedback()
+{
+	$db = DB::getConnection();
+
+	if( has_table( 'spiza_feedback' ) )
+		exit( 'Tablica spiza_feedback vec postoji. Obrisite ju pa probajte ponovno.' );
+
+	try
+	{
+		$st = $db->prepare( 
+			'CREATE TABLE IF NOT EXISTS spiza_feedback (' .
+			'id int NOT NULL PRIMARY KEY AUTO_INCREMENT,' .
+			'id_user int NOT NULL,' .
+			'id_restaurant NOT NULL,' .
+			'content varchar(1000) NOT NULL,' .
+			'thumbs_up int NOT NULL,' .
+			'thumbs_down int NOT NULL)'		
+		);
+
+		$st->execute();
+	}
+	catch( PDOException $e ) { exit( "PDO error [create spiza_restaurants]: " . $e->getMessage() ); }
+
+	echo "Napravio tablicu spiza_restaurants.<br />";
+}
+
+function create_table_orders()
+{
+	$db = DB::getConnection();
+
+	if( has_table( 'spiza_orders' ) )
+		exit( 'Tablica spiza_orders vec postoji. Obrisite ju pa probajte ponovno.' );
+
+	try
+	{
+		$st = $db->prepare( 
+			'CREATE TABLE IF NOT EXISTS spiza_orders (' .
+			'id int NOT NULL PRIMARY KEY AUTO_INCREMENT,' .
+			'id_user int NOT NULL,' .
+			'id_restaurant NOT NULL,' .
+			'id_food NOT NULL,' .
+			'how_many_times int NOT NULL)'		
+		);
+
+		$st->execute();
+	}
+	catch( PDOException $e ) { exit( "PDO error [create spiza_orders]: " . $e->getMessage() ); }
+
+	echo "Napravio tablicu spiza_orders.<br />";
+}
 
 
 ?> 
