0c206f72715ba693c07356762212e7abd6c3e136
Marti1902 <martina.gacina@gmail.com>
2020-07-06 23:56:40 +0200

    status moje narudzbe, dodana cijena i cijena s popustom (provjerit kako je popusta zapravo napravljen

--- view/user_orders.php
+++ view/user_orders.php
@@ -5,11 +5,21 @@ Popis aktivnih narudžbi:
     <?php 
     foreach( $orderList as $order ){
         if ( $order[0]->active != 0 ){
+            if( $order[0]->active == 1 )
+                $status = 'Narudžba poslana. Čeka se odgovor restorana.';
+            else if( $order[0]->active == 2 )
+                $status = 'Restoran je prihvatio Vašu narudžbu.';
+            else if( $order[0]->active == 3 )
+                $status = 'Restoran je prihvatio Vašu narudžbu. Procijenjeno vrijeme čekanja je ' . strtotime( $order[0]->delivery_time ) . '.';
+        
             echo '<li>' .
                 'Iz ' . $order[0]->id_restaurant . ':<br>' .
-                'Status narudžbe: ' . $order[0]->active . '<br>';
+                'Status narudžbe: ' . $status . '<br>';
             foreach ( $order[1] as $food )
                 echo $food[0]->name . ', količina: ' . $food[1] . '<br>';
+            echo 'Ukupna cijena: ' . $order[0]->price_total . '<br>';
+            $spopustom = $order[0]->price_total - $order[0]->discount;
+            echo 'Cijena s popustom: ' . $spopustom . '<br>';
             echo '</li>';
         }
     }
@@ -25,6 +35,9 @@ Popis dostavljenih narudžbi:
                 'Iz ' . $order[0]->id_restaurant . ':<br>';
             foreach ( $order[1] as $food )
                 echo $food[0]->name . ', količina: ' . $food[1] . '<br>';
+            echo 'Ukupna cijena: ' . $order[0]->price_total . '<br>';
+            $spopustom = $order[0]->price_total - $order[0]->discount;
+            echo 'Cijena s popustom: ' . $spopustom . '<br>';
             if( $order[0]->rating != 1 && $order[0]->rating != 2 && $order[0]->rating != 3 && $order[0]->rating != 4 && $order[0]->rating != 5 && $order[0]->rating != 6 && $order[0]->rating != 7 && $order[0]->rating != 8 && $order[0]->rating != 9 && $order[0]->rating != 10)
                 echo "<button class='ocijeni' id='" . $order[0]->id_order . "'>Ocijeni</button>";
             echo '</li>';
