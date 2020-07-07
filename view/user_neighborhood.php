b716e108902aebb77cb396eb74b567b3146fc083
manuelaplesa96 <manuela.plesa@gmail.com>
2020-07-07 22:14:35 +0200

    Dodana navigacija

--- view/user_neighborhood.php
+++ view/user_neighborhood.php
@@ -13,4 +13,16 @@
 </div>
 </div>
 
+<!-- <h4>Istraži više restorana:</h4> -->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">   
+    <ul class="nav navbar-nav">
+        <li class="nav-item"><a class="nav-link" href="<?php echo __SITE_URL; ?>/index.php?rt=user/popular">Popularni</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?php echo __SITE_URL; ?>/index.php?rt=user/restaurants">Svi restorani</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?php echo __SITE_URL; ?>/index.php?rt=user/neighborhood">U kvartu</a></li>
+        <li class="nav-item"><a class="nav-link" href="<?php echo __SITE_URL; ?>/index.php?rt=user/foodType">Prema vrsti hrane</a></li>
+    </ul>
+</nav> 
+
+<!-- <div style="height: 250px;"> </div> -->
+
 <?php require_once __DIR__ . '/header&footer/_footer.php'; ?>
