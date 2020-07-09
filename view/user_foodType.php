1f899a78fcfcdfb9bd45a54be8767fce0a21a3db
Marti1902 <martina.gacina@gmail.com>
2020-07-09 22:02:43 +0200

    prikaz po tipu hrane

--- view/user_foodType.php
+++ view/user_foodType.php
@@ -1,14 +1,18 @@
 <?php require_once __DIR__ . '/header&footer/_header.php'; ?>
 
-<div class="list-group">
+<br>
+<div class="container" style="width: 75%;">
     <?php 
         if( $foodType != [] ){
             foreach( $foodType as $food ){ //u css-u maknuti točkice kod liste
-                echo '<a class="list-group-item list-group-item-action" href="index.php?rt=user/restaurantsByFoodType&id_foodType=' . $food->id_foodType . '">';
-                echo ucfirst( $food->name );
+                echo '<a style="display:block" href="index.php?rt=user/restaurantsByFoodType&id_foodType=' . $food->id_foodType . '">';
+                echo '<div class="card">';
                 if( $food->image_path !== null )
-                    echo '<img src="'. __SITE_URL . $food->image_path .'"width="100", height="100">';
-                echo '</a>';
+                    echo '<img class="card-img-top" src="'. __SITE_URL . $food->image_path .'" style="width:50%; margin: auto;">';
+                
+                echo '<div class="card-body" style="text-align: center">
+                    <h4 class="card-title" style="color: black;">' . ucfirst( $food->name ) . '</h4></div>';
+                echo '</div></a>';
             }
         }
         else echo '<div class="list-group-item">Trenutno nema raspoloživih vrsta hrane</div>';
